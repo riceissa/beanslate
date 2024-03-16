65fc32058e29cd366786bd55e3c23a89224290c1
Issa Rice <riceissa@gmail.com>
2024-03-15 23:12:46 -0700

    deduplicate account types/transaction keywords

--- beanslate.py
+++ beanslate.py
@@ -3,60 +3,58 @@
 import re
 import sys
 
+SIGN_MAP = {
+    "Assets": {
+        "increase": "+",
+        "decrease": "-",
+        "paid": "-",
+        "spend": "-",
+        "owed to me": "+",
+        "received": "+",
+        "receive": "+",
+        "repayment to me": "-",
+        "repayment to them": "+",
+        "opening balance": "+",
+        # "payment": "-",
+    },
+    "Liabilities": {
+        "increase": "-",
+        "decrease": "+",
+        "owed to me": "+",
+        "charge": "-",
+        "payment": "+",
+        "repayment to them": "+",
+    },
+    "Equity": {
+        "opening balance": "-",
+    },
+    "Income": {
+        "owed to me": "-",
+        "earned": "-",
+    },
+    "Expenses": {
+        "spent": "+",
+        "rebate": "-",
+    }
+}
+
 def figure_out_debit_credit(sign):
     return "Debit" if sign == "+" else "Credit"
 
 def figure_out_sign(account_type, transaction_keyword):
     try:
-        if account_type == "Assets":
-            return {
-                    "increase": "+",
-                    "decrease": "-",
-                    "paid": "-",
-                    "spend": "-",
-                    "owed to me": "+",
-                    "received": "+",
-                    "receive": "+",
-                    "repayment to me": "-",
-                    "repayment to them": "+",
-                    "opening balance": "+",
-                    # "payment": "-",
-                    }[transaction_keyword]
-        if account_type == "Liabilities":
-            return {
-                    "increase": "-",
-                    "decrease": "+",
-                    "owed to me": "+",
-                    "charge": "-",
-                    "payment": "+",
-                    "repayment to them": "+",
-                    }[transaction_keyword]
-        if account_type == "Equity":
-            return {
-                    "opening balance": "-",
-                    }[transaction_keyword]
-        if account_type == "Income":
-            return {
-                    "owed to me": "-",
-                    "earned": "-",
-                    }[transaction_keyword]
-        if account_type == "Expenses":
-            return {
-                    "spent": "+",
-                    "rebate": "-",
-                    }[transaction_keyword]
+        return SIGN_MAP[account_type][transaction_keyword]
     except KeyError:
         print(f"Can't use {transaction_keyword} for {account_type} accounts!",
               file=sys.stderr)
         sys.exit()
 
 for line in sys.stdin:
-    account_types = ["Assets", "Liabilities", "Equity", "Income", "Expenses"]
+    account_types = list(SIGN_MAP.keys())
     account_types_re = "|".join(account_types)
-    transaction_keywords = ["spent", "charge", "paid", "decrease",
-            "increase", "owed to me", "owed to them", "rebate", "earned",
-            "received", "repaid to me", "repaid to them", "repayment to me",
-            "repayment to them", "payment", "opening balance"]
+    transaction_keywords = []
+    for v in SIGN_MAP.values():
+        transaction_keywords.extend(v.keys())
     m = re.match(rf"\s+({account_types_re}):", line)
     if m:
         account_type = m.group(1)
@@ -70,6 +68,7 @@ for line in sys.stdin:
                   file=sys.stderr, end="")
             sys.exit()
         sign = figure_out_sign(account_type, transaction_keyword)
+        assert sign in ["+", "-"]
         debit_or_credit = figure_out_debit_credit(sign)
         if sign == "-":
             print(re.sub(r"(\d+\.\d+)", fr"-\1  ; {debit_or_credit}: ", line), end="")
